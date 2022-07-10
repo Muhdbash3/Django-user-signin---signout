52d7e6c4f68a749c83ad0ba4c875595cf375bc6f
Awusa Collins Ekene <58849534+Collinshack@users.noreply.github.com>
2022-07-10 17:43:34 +0100

    Update models.py
    After adding the models, don't forget to make migrations and register your model in admin.py folder

--- login/models.py
+++ login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SignupDetails(models.Model):
+  username = models.CharField(max_length=100)
+  firstName = models.CharField(max_length=30)
+   lastName = models.CharField(max_length=30)
+     Email = models.EmailField(max_length=50)
+      password1 = models.CharField(max_length=50)
+      password2 = models.CharField(max_length=50)
+      
