aed3a6ae7d41b6505f9e83fa851be29d112b7094
Muhammad Basheer Yahya <101598696+Muhdbash3@users.noreply.github.com>
2022-09-22 20:17:40 +0100

    Update models.py

--- login/models.py
+++ login/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class SignupDetails(models.Model):
   username = models.CharField(max_length=100)
-  firstName = models.CharField(max_length=30)
-   lastName = models.CharField(max_length=30)
-     Email = models.EmailField(max_length=50)
-      password1 = models.CharField(max_length=50)
-      password2 = models.CharField(max_length=50)
+  fname = models.CharField(max_length=30)
+  lname = models.CharField(max_length=30)
+  Email = models.EmailField(max_length=50)
+  pass1 = models.CharField(max_length=50)
+  pass2 = models.CharField(max_length=50)
       
