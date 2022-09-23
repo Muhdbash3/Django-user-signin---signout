0e0f73e3c0a4a646b5123499c98297641dfe112f
Muhammad Basheer Yahya <101598696+Muhdbash3@users.noreply.github.com>
2022-09-23 08:05:57 +0100

    Update models.py

--- login/models.py
+++ login/models.py
@@ -5,7 +5,7 @@ class SignupDetails(models.Model):
   username = models.CharField(max_length=100)
   fname = models.CharField(max_length=30)
   lname = models.CharField(max_length=30)
-  Email = models.EmailField(max_length=50)
+  email = models.EmailField(max_length=50)
   pass1 = models.CharField(max_length=50)
   pass2 = models.CharField(max_length=50)
       
